926945cec29149c1d561a8e9722081b91b3d5020
Leonardo Lonque <leonardoarrudalonque@outlook.com>
2026-09-23 12:09:20 -0300

    Ajustando gestaoDocumentos

--- src/gestaoLicencas/gestaoLicencas.module.ts
+++ src/gestaoLicencas/gestaoLicencas.module.ts
@@ -1,15 +1,13 @@
-import { AuthModule } from "@/auth/auth.module";
-import { Module } from "@nestjs/common";
-import { GestaoLicencasController } from "./gestaoLicencas.controller";
-import { GestaoLicencasService } from "./gestaoLicencas.service";
-import { PrismaService } from "@/prisma/prisma.service";
+import { AuthModule } from '@/auth/auth.module';
+import { Module } from '@nestjs/common';
+import { GestaoLicencasController } from './gestaoLicencas.controller';
+import { GestaoLicencasService } from './gestaoLicencas.service';
+import { PrismaService } from '@/prisma/prisma.service';
+import { LicencaNotificacaoService } from './licencaNotificacao.service';
 
 @Module({
-    imports: [AuthModule],
-    controllers: [GestaoLicencasController],
-    providers: [
-        GestaoLicencasService,
-        PrismaService,
-    ]
+  imports: [AuthModule],
+  controllers: [GestaoLicencasController],
+  providers: [GestaoLicencasService, LicencaNotificacaoService, PrismaService],
 })
 export class GestaoLicencasModule {}
